fa0c96d03ba38a6e181d7ca3e20b166021a88a43
Rauf <r.wirapandita@gmail.com>
2023-09-13 11:47:23 +0700

    Create User services

---
+++ src/main/java/com/smk/cashier/model/Stock.java
@@ -0,0 +1,10 @@
+package com.smk.cashier.model;
+
+import sun.text.normalizer.NormalizerBase;
+
+public class Stock extends Model {
+    private int id;
+    private String kodeBarang;
+    private int stokBarang;
+
+}
